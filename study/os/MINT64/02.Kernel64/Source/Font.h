fda12ca76c48af35b9ec11a8badead4bb408e83a
le3mon <okko2929@gmail.com>
2023-01-25 15:05:29 +0900

    35 Chapter Debuging

---
+++ study/os/MINT64/02.Kernel64/Source/Font.h
@@ -0,0 +1,12 @@
+#ifndef __FONT_H__
+#define __FONT_H__
+
+// 매크로와 전역 변수
+// 영문 폰트의 너비와 길이
+#define FONT_ENGLISHWIDTH   8
+#define FONT_ENGLISHHEIGHT  16
+
+// 영문 비트맵 폰트 데이터
+extern unsigned char g_vucEnglishFont[];
+
+#endif
