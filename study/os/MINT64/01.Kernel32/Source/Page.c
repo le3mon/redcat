3adcff94f816c7be4b550f17c872a2ccd0162e73
leemon <okko2929@gmail.com>
2023-02-28 05:36:04 +0900

    48 Chap Complete

--- study/os/MINT64/01.Kernel32/Source/Page.c
+++ study/os/MINT64/01.Kernel32/Source/Page.c
@@ -17,16 +17,16 @@ void kInitializePageTables(void) {
     pstPML4TEntry = ( PML4TENTRY* ) 0x100000;
     kSetPageEntryData( &( pstPML4TEntry[ 0 ] ), 0x00, 0x101000, PAGE_FLAGS_DEFAULT | PAGE_FLAGS_US, 0 );
     for( i = 1 ; i < PAGE_MAXENTRYCOUNT ; i++ )
-        kSetPageEntryData( &( pstPML4TEntry[ i ] ), 0, 0, 0, 0 );
+        kSetPageEntryData(( pstPML4TEntry + i), 0, 0, 0, 0 );
 
     // 페이지 디렉터리 포인터 테이블 생성
     // 64개 엔트리를 설정하여 64GB까지 매핑
     pstPDPTEntry = ( PDPTENTRY* ) 0x101000;
     for( i = 0 ; i < 64 ; i++ )
-        kSetPageEntryData( &( pstPDPTEntry[ i ] ), 0, 0x102000 + ( i * PAGE_TABLESIZE ), PAGE_FLAGS_DEFAULT, 0 );
+        kSetPageEntryData((pstPDPTEntry + i), 0, 0x102000 + ( i * PAGE_TABLESIZE ), PAGE_FLAGS_DEFAULT | PAGE_FLAGS_US, 0 );
     
     for( i = 64 ; i < PAGE_MAXENTRYCOUNT ; i++ )
-        kSetPageEntryData( &( pstPDPTEntry[ i ] ), 0, 0, 0, 0 );
+        kSetPageEntryData((pstPDPTEntry + i), 0, 0, 0, 0 );
     
     // 페이지 디렉터리 테이블 생성
     // 하나의 페이지 디렉터리가 1GB까지 매핑 가능
@@ -55,5 +55,5 @@ void kInitializePageTables(void) {
 
 void kSetPageEntryData(PTENTRY* pstEntry, DWORD dwUpperBaseAddress, DWORD dwLowerBaseAddress, DWORD dwLowerFlags, DWORD dwUpperFlags) {
     pstEntry->dwAttributeAndLowerBaseAddress = dwLowerBaseAddress | dwLowerFlags;
-    pstEntry->dwUpperBaseAddressAndEXB = ( dwUpperBaseAddress & 0xFF ) | dwUpperFlags;
+    pstEntry->dwUpperBaseAddressAndEXB = dwUpperBaseAddress | dwUpperFlags;
 }
