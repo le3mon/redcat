d6833189e6b624bdabfa0721befd56513961be43
le3mon <okko2929@gmail.com>
2023-07-13 16:07:00 +0900

    Doing Chapter 4

--- study/cpu/nand2tetris/projects/04/fill/Fill.asm
+++ study/cpu/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,40 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    @n
+    M=0
+
+    @32
+    D=A
+    @R0
+    M=D
+
+
+// (RESET)
+//     @SCREEN
+//     D=M
+//     @R0
+//     M=D
+
+(LOOP)
+    @SCREEN
+    D=A
+    
+    @n
+    A=D+M
+    M=-1
+
+
+    @R0
+    A=D+M
+    M=-1
+
+    // @64
+    // A=D+M
+    // M=-1
+
+    @n
+    M=M+1
+
+    @LOOP
+    0;JMP
